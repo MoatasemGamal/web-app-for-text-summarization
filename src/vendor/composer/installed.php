c6f7aca192cdccd661a86d22e3417944c6910692
MoatasemGamal <moatasemco@gmail.com>
2024-06-30 09:56:53 +0300

    upload vendor folder

---
+++ src/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'daghfal/daghfal',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b79c9c7379d5d143fbf876cd4c3f2de8451a00f4',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'daghfal/daghfal' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b79c9c7379d5d143fbf876cd4c3f2de8451a00f4',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
